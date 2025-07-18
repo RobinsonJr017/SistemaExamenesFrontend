4c486987a884ea0db8c165abc995d1f561e31b38
Robinson Lopez <robins.616009@gmail.com>
2025-07-17 19:57:30 -0500

    Creando componentes signupde registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SignupComponent } from "./pages/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, NavbarComponent, RouterOutlet, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
