970fd10cf34e76e3c0854d92cd844f4e017a96d6
Robinson Lopez <robins.616009@gmail.com>
2025-08-01 17:22:40 -0500

    Diseño formulario signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { SignupComponent } from "./pages/signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, NavbarComponent, RouterOutlet, SignupComponent],
+  imports: [NavbarComponent, RouterOutlet, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
