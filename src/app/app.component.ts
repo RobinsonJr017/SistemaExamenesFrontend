4aff6d59578c2e5a096f914515284e0c68a156f7
Robinson Lopez <robins.616009@gmail.com>
2025-08-06 20:04:24 -0500

    Mejora (Diseño Formulario Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { SignupComponent } from "./pages/signup/signup.component";
 import { HttpClientModule } from '@angular/common/http';
-//import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet, SignupComponent, HttpClientModule, /*MatSnackBarModule*/],
+  imports: [NavbarComponent, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // Importa esto
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
-    provideHttpClient() // Añade esto
+    provideHttpClient(),
+    provideRouter(routes)
   ]
 }).catch((err) => console.error(err));
