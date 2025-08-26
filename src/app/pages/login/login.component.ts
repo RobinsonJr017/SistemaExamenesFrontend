9ab6ff24c8acbcfa30fc24b8bbe9a58b2a4eca67
Robinson Lopez <robins.616009@gmail.com>
2025-08-26 16:53:50 -0500

    Agregando validaciones al formulario de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,31 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginData = {
+    "username" : '',
+    "password" : ''
+  }
+
+  constructor(private snack:MatSnackBar){}
+
+  ngOnInit(): void {      
+  }
+
+  formSubmit(){
+    if(this.loginData.username.trim() == '' || this.loginData.username.trim() == null){
+      this.snack.open('El nombre de usuario es requerido!!','Aceptar',{
+        duration: 3000
+      })
+      return;
+    }
+    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null){
+      this.snack.open('La contraseña es requerida!!','Aceptar',{
+        duration: 3000
+      })
+      return;
+    }
+    console.log("Click en el boton de login");
+  }
 }
