7dcd934801e51e2a162527414cc3addc39b26d98
Robinson Lopez <robins.616009@gmail.com>
2025-08-28 16:48:23 -0500

    Creacion de metodos para obtenerUsuario(login)

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -13,4 +13,49 @@ export class LoginService {
   public generateToken(loginData:any){
     return this.http.post(`${baserUrl}/generate-token`, loginData);
   }
+
+  //Iniciamos sesion y establecemos el token en el localStorage
+  public loginUser(token:any){
+    localStorage.setItem('token', token);
+  }
+
+  public isLoggedIn(){
+    let tokenStr = localStorage.getItem('token');
+    if(tokenStr == undefined || tokenStr == '' || tokenStr == null){
+      return false;
+    }else{
+      return true;
+    }
+  }
+  
+  //cerramos sesion y elimnamos el token del localStorage
+  public logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  //Obtenemos el token
+  public getToken(){
+    return localStorage.getItem('token');
+  }
+
+  public setUser(user:any){
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUser(){
+    let userStr = localStorage.getItem('user');
+    if(userStr != null){
+      return JSON.parse(userStr);
+    }else{
+      this.logout();
+      return null;
+    }
+  }
+
+  public getUserRole(){
+    let user = this.getUser();
+    return user.authorities[0].authority;    
+  }
 }
