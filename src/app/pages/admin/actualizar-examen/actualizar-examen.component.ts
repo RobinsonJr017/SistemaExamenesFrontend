5c5bafee9ef7eb8ceec69eb51aac57f1b638094b
Robinson Lopez <robins.616009@gmail.com>
2026-05-19 18:53:08 -0500

    Impl funcion actualizar examen

--- src/app/pages/admin/actualizar-examen/actualizar-examen.component.ts
+++ src/app/pages/admin/actualizar-examen/actualizar-examen.component.ts
@@ -1,20 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ExamenService } from '../../../services/examen.service';
+import { CategoriaService } from '../../../services/categoria.service';
+import { FormsModule } from "@angular/forms";
+import { MatCard } from "@angular/material/card";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatSelectModule } from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { NgIf } from '@angular/common';
+import { MatButton } from "@angular/material/button"; 
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-actualizar-examen',
-  imports: [],
+  imports: [NgIf, FormsModule, MatCard, MatFormField, MatInput, MatSelectModule, MatSlideToggleModule, MatButton],
   templateUrl: './actualizar-examen.component.html',
   styleUrl: './actualizar-examen.component.css'
 })
 export class ActualizarExamenComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute) {}
+  constructor(
+    private route:ActivatedRoute, 
+    private examenService:ExamenService,
+    private categoriaService:CategoriaService,
+    private  router:Router) {}
 
   examenId = 0;
+  examen:any;
+  categorias:any;
 
   ngOnInit(): void {
       this.examenId = this.route.snapshot.params['examenId'];
-      alert(this.examenId);
+      this.examenService.obtenerExamen(this.examenId).subscribe(
+        (data) => {
+          this.examen = data;
+          console.log(this.examen);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+
+      this.categoriaService.listarCategorias().subscribe(
+        (data:any) => {
+          this.categorias = data;
+        },
+        (error) => {
+          alert('Error al cargar las categorias')
+        }
+      )
+  }
+
+  public actualizarDatos(){
+    this.examenService.actualizarExamen(this.examen).subscribe(
+      (data) => {
+        Swal.fire('Examen actualizado','El examen ha sido actualizado con exito','success').then(
+          (e) => {
+            this.router.navigate(['/admin/examenes']);
+          }
+        );
+      },
+      (error) => {
+        Swal.fire('Error en el sistema','No se ha podido actualizar el examen','error');
+        console.log(error);        
+      }
+    )
   }
 }
--- src/app/services/examen.service.ts
+++ src/app/services/examen.service.ts
@@ -14,10 +14,18 @@ export class ExamenService {
   }
 
   public agregarExamen(examen:any){
-    return this.http.post(`${baserUrl}/examen/`, examen)
+    return this.http.post(`${baserUrl}/examen/`, examen);
   }
 
   public eliminarExamen(examenId:any){
-    return this.http.delete(`${baserUrl}/examen/${examenId}`)
+    return this.http.delete(`${baserUrl}/examen/${examenId}`);
+  }
+
+  public obtenerExamen(examenId:any){
+    return this.http.get(`${baserUrl}/examen/${examenId}`);
+  }
+
+  public actualizarExamen(examen:any){
+    return this.http.put(`${baserUrl}/examen/`, examen);
   }
 }
